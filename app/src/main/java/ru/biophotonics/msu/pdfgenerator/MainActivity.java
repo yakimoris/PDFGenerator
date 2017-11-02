7fec886568794eba0848fd7bf10116ef067ddcf8
byakimov <bp.yakimov@gmail.com>
2017-11-03 02:39:08 +0300

    removed unneccessary permissions

--- app/src/main/java/ru/biophotonics/msu/pdfgenerator/MainActivity.java
+++ app/src/main/java/ru/biophotonics/msu/pdfgenerator/MainActivity.java
@@ -17,12 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
 
     private final static int REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION_RESULT = 0;
-    private static final int REQUEST_READ_EXTERNAL_STORAGE_PERMISSION_RESULT = 1;
 
     private static final String fileName = "test";
     private static final String Message = "This is message!";
     private PdfGenerator mPdfGenerator;
 
+    /**
+     * checking for permission and creating mPdfGenerator object
+     */
     private void checkWriteStoragePermission() {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if((ContextCompat.checkSelfPermission(MainActivity.this,
@@ -41,26 +43,6 @@ public class MainActivity extends AppCompatActivity {
                 }
                 requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                         REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION_RESULT);
-            }
-        }
-    }
-
-
-    private void checkReadStoragePermission() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if((ContextCompat.checkSelfPermission(MainActivity.this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_GRANTED)) {
-
-
-            } else {
-                if(shouldShowRequestPermissionRationale(
-                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                    Toast.makeText(MainActivity.this,
-                            "app needs to be able to save pdfs", Toast.LENGTH_SHORT).show();
-                }
-                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                        REQUEST_READ_EXTERNAL_STORAGE_PERMISSION_RESULT);
             }
         }
     }
@@ -89,38 +71,27 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
-
-                return;
-            }
-
-            case (REQUEST_READ_EXTERNAL_STORAGE_PERMISSION_RESULT):{
-                if (grantResults.length != 1 || grantResults[0]
-                        != PackageManager.PERMISSION_GRANTED) {
-                    showToast("Permission not granted!");
-                    showToast("Exiting ...");
-
-                    try {
-                        wait(10000);
-                        finish();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
             }
         }
     }
 
+    /**
+     * showing Toast with message
+     * @param message
+     */
     private void showToast(String message){
         Toast.makeText(getApplicationContext(), message,Toast.LENGTH_SHORT).show();
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //check for permission
         checkWriteStoragePermission();
-//        checkReadStoragePermission();
 
+        //generates PDF
         mPdfGenerator.generatePDF();
     }
 }
